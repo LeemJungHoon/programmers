037ca31548591883dc88e86a7218fea94db43723
LeemJungHoon <xjqhdjq@naver.com>
2025-01-10 12:31:44 +0900

    [level 1] Title: 바탕화면 정리, Time: 8.50 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,38 @@
+function solution(wallpaper) {
+    //30분
+    const obj = {
+        fileState : [],
+    };
+    let top = [];
+    let left = [];
+    let right = [];
+    let btm = [];
+    let dragStart = [];
+    let dragEnd = [];
+    let hang = 0;
+    
+    wallpaper.forEach((item,idx) => {
+        [...item].forEach((file,idx) => {
+            if(file === "#"){
+                obj.fileState.push([hang,idx]);
+            }
+        })
+        hang++;
+    })
+    
+    top = obj.fileState[0];
+    btm = obj.fileState[obj.fileState.length-1];
+    
+    obj.fileState.sort(([a,b],[c,d]) => {
+        return [b] - [d];
+    })
+    
+    left = obj.fileState[0];
+    right = obj.fileState[obj.fileState.length-1];
+    
+    dragStart = [top[0],left[1]];
+    dragEnd = [btm[0]+1,right[1]+1];
+    
+    return [top[0],left[1],btm[0]+1,right[1]+1];
+    
+}
