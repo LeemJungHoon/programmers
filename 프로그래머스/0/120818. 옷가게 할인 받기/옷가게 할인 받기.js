ed3227a58d8d4c12bd1fbacbc4d69ea6bf05ee72
LeemJungHoon <xjqhdjq@naver.com>
2025-06-08 15:35:22 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,16 @@
+function solution(price) {
+    if(price < 100000){
+        return price;
+    }
+    
+    if(price >= 100000 && price < 300000){
+        return Math.trunc(price - price*0.05);
+    }else if(price >= 300000 && price < 500000){
+        return Math.trunc(price - price*0.1);
+    }else if(price >= 500000){
+        return Math.trunc(price - price*0.2);
+    }else{
+        
+    }
+        
+}
