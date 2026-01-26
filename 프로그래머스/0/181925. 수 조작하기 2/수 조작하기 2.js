fa902aeca45eaa5f980aae258207c19435879c8f
LeemJungHoon <xjqhdjq@naver.com>
2026-01-26 15:36:40 +0900

    [level 0] Title: 수 조작하기 2, Time: 9.46 ms, Memory: 41.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,17 @@
+function solution(numLog) {
+    
+    const obj = {
+        "1" : "w",
+        "-1" : "s",
+        "10" : "d",
+        "-10" : "a",
+    }
+    
+    let result = "";
+    
+    for(let i=1; i<numLog.length; i++){
+        result += obj[numLog[i] - numLog[i-1] + ""]
+    }
+    
+    return result;
+}
