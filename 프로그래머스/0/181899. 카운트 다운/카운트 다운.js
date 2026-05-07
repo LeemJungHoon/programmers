0dabd5bc9bd61ebb1592681716ff2abd9dc26af1
LeemJungHoon <xjqhdjq@naver.com>
2026-05-07 15:50:32 +0900

    [level 0] Title: 카운트 다운, Time: 0.05 ms, Memory: 32.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -0,0 +1,10 @@
+function solution(start_num, end_num) {
+    
+    let arrResult = [];
+    
+    for(let i=start_num; i>=end_num; i--){
+        arrResult.push(i);
+    }
+    
+    return arrResult;
+}
