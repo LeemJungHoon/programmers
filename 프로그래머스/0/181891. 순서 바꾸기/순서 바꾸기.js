af6c4cefe4d2a0e43aecd156d18be1d15feb78da
LeemJungHoon <xjqhdjq@naver.com>
2026-05-13 19:12:29 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.04 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,12 +1,4 @@
 function solution(num_list, n) {
-    
-    let arr_firstWord = [];
-    let arr_secondWord = [];
-    
-    num_list.forEach((item, idx) => {
-        idx < n ? arr_secondWord.push(item) : arr_firstWord.push(item);
-    })
-    arr_firstWord.push(...arr_secondWord);
-    
-    return arr_firstWord;
+    num_list.push(...num_list.splice(0,n));
+    return num_list
 }
