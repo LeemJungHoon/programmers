2c1d944a34e27be9849ddf19737844125db279f6
LeemJungHoon <xjqhdjq@naver.com>
2026-05-13 19:48:45 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,19 +1,7 @@
 function solution(str_list) {
-    let arr_result = [];
-    let cnt = 0;
-    
-    while(cnt <= str_list.length){
-        
-        if(str_list[cnt] === "l"){
-            arr_result.push(...str_list.slice(0,cnt));
-            break;
-        }else if(str_list[cnt] === "r"){
-            arr_result.push(...str_list.slice(cnt+1));
-            break;
-        }
-        
-        cnt++;
+    for(let i=0; i<=str_list.length; i++){
+        if(str_list[i] === "l") return str_list.slice(0,i);
+        if(str_list[i] === "r") return str_list.slice(i+1);
     }
-    
-    return arr_result;
+    return [];
 }
