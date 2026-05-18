1326c2086796ca2184b415e4d0eec727a950eddf
LeemJungHoon <xjqhdjq@naver.com>
2026-05-18 19:20:00 +0900

    [level 1] Title: 실패율, Time: 871.17 ms, Memory: 39.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -5,20 +5,16 @@ function solution(N, stages) {
         obj_result[i] ? null : obj_result[i]=0;
     }
     
-    const arr_result = Object.entries(obj_result);
-    
-    //이제 각각 스테이지에 실패율을 넣을것임
-    Object.entries(obj_result).map(([sNum,_], idx) => {
+    for(let i=1; i<=N; i++){
         let reachedPlayers = 0;
         let notClearPlayers = 0;
         
         stages.forEach((item,idx) => {
-            if(item === +sNum) notClearPlayers++;
-            if(item >= +sNum) reachedPlayers++;
-            
+            if(item === i) notClearPlayers++;
+            if(item >= i) reachedPlayers++;
         })
-        obj_result[sNum] = (notClearPlayers/reachedPlayers);
-    })
+        obj_result[i] = (notClearPlayers/reachedPlayers);   
+    }
     
     return Object.entries(obj_result)
         .sort(([a,b],[c,d]) => d-b)
