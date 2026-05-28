692c629e4351627a260b4d187ddd7ee761727cc7
LeemJungHoon <xjqhdjq@naver.com>
2026-05-28 20:29:46 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.09 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,21 +1,5 @@
 function solution(myString, pat) {
     
-    
-    let arr_myString = [...myString.toLowerCase()];
-    let arr_pat = [...pat.toLowerCase()];
-    
-    if(arr_myString.length < arr_pat.length){
-        return 0;
-    }
-    
-    return +arr_myString.some((item,idx) => {
-       if(item === arr_pat[0]){
-            let word1 = arr_myString.slice(idx,idx+arr_pat.length);
-             return (word1.length === arr_pat.length && word1.every((item,idx) => item === arr_pat[idx])) ? 1 : 0
-        } 
-    });
-    
-    
-    
+    return myString.toUpperCase().includes(pat.toUpperCase()) ? 1 :0
     
 }
