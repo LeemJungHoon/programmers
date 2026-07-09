2adcac9d78357f7d6ac0da5484bfab788cfd672f
LeemJungHoon <xjqhdjq@naver.com>
2026-07-09 11:32:35 +0900

    [level 0] Title: 정수 찾기, Time: 0.05 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
@@ -1,10 +1,4 @@
 function solution(num_list, n) {
     
-    let bTrue = false
-    
-    num_list.forEach((item) => {
-        item === n ? bTrue = true : null;
-    })
-    
-    return +bTrue;
+    return +num_list.includes(n);
 }
