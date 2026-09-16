4ef4ced73468a1af09e52751c459c4bb71f2d8be
LeemJungHoon <xjqhdjq@naver.com>
2026-09-16 15:23:00 +0900

    [level 1] Title: 실패율, Time: 6.56 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,23 +1,34 @@
 function solution(N, stages) {
-    const obj_result = {};
+    //2시 20분 시작
+    //스테이지 돌면서, 각 아이템이
+    //obj만들고, 스테이지 돌면서 각 아이템 = 아이템+ 하면 스테이지에 도달한 값 나오지?
+    //그리고  obj 돌면서 0인거 제외하고
+    
+    //
+    const obj = {};
+    const reachedPlayer = [];
+    let stageLength = stages.length;
+    let num = 0;
+    let cnt = 1;
     
     for(let i=1; i<=N; i++){
-        obj_result[i] ? null : obj_result[i]=0;
+        obj[i] = 0;
     }
     
-    for(let i=1; i<=N; i++){
-        let reachedPlayers = 0;
-        let notClearPlayers = 0;
+    stages.forEach((item) => {
         
-        stages.forEach((item,idx) => {
-            if(item === i) notClearPlayers++;
-            if(item >= i) reachedPlayers++;
-        })
-        obj_result[i] = (notClearPlayers/reachedPlayers);   
+        item <= N ? obj[item] += 1 : 0;
+    })
+    
+    for(let i in obj){
+        reachedPlayer.push([cnt,(obj[i]/(stageLength-num))]);
+        num += obj[i];
+        cnt++;
     }
     
-    return Object.entries(obj_result)
-        .sort(([a,b],[c,d]) => d-b)
-        .map(([a,b]) => {return +a});
+    reachedPlayer.sort(([a,b],[c,d]) => d-b)
+    
+    return reachedPlayer.map(([a,b]) => a)
+    
     
 }
