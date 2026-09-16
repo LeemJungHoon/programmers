c9d4fff89c722c0a779943a130f5d26ad4cab406
LeemJungHoon <xjqhdjq@naver.com>
2026-09-16 15:24:13 +0900

    [level 1] Title: 실패율, Time: 2.61 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,10 +1,4 @@
 function solution(N, stages) {
-    //2시 20분 시작
-    //스테이지 돌면서, 각 아이템이
-    //obj만들고, 스테이지 돌면서 각 아이템 = 아이템+ 하면 스테이지에 도달한 값 나오지?
-    //그리고  obj 돌면서 0인거 제외하고
-    
-    //
     const obj = {};
     const reachedPlayer = [];
     let stageLength = stages.length;
